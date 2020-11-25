4156b2949e3b4006a4577ec502b9bc1c7513f214
shycastle <shynycastle@naver.com>
2020-11-25 14:08:48 +0900

    지도 띄우고 줌 기능+  다중 마커 달기

--- app/src/main/java/org/techtown/chabak/MainActivity.java
+++ app/src/main/java/org/techtown/chabak/MainActivity.java
@@ -5,6 +5,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -30,22 +31,30 @@ public class MainActivity extends AppCompatActivity
     public void onMapReady(final GoogleMap googleMap) {
 
         mMap = googleMap;
-
+        //서울의 위도 경도
         LatLng SEOUL = new LatLng(37.56, 126.97);
-
+        LatLng GwangJu =new LatLng(35.180019,126.875832);
+        LatLng start= new LatLng(36.558132, 127.905404);
+        MarkerOptions markerOptions1= new MarkerOptions();
+        markerOptions1.position(GwangJu);
+        markerOptions1.title("광주");
+        markerOptions1.snippet("우리집이다 이색갸~");
+        mMap.addMarker(markerOptions1);
+        //마커 옵션
         MarkerOptions markerOptions = new MarkerOptions();
         markerOptions.position(SEOUL);
         markerOptions.title("서울");
         markerOptions.snippet("한국의 수도");
         mMap.addMarker(markerOptions);
 
+        //줌 인 1,5,10 ,20 단위였던듯
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(start, 7));
 
-        // 기존에 사용하던 다음 2줄은 문제가 있습니다.
-
-        // CameraUpdateFactory.zoomTo가 오동작하네요.
-        //mMap.moveCamera(CameraUpdateFactory.newLatLng(SEOUL));
-        //mMap.animateCamera(CameraUpdateFactory.zoomTo(10));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(SEOUL, 10));
+        //줌 버튼 생성
+        UiSettings mapUiSettings = mMap.getUiSettings ();
+        mapUiSettings.setZoomControlsEnabled (true);
+        //나침반 생성 안돼 왜지? 괜찮아 필요없어
+        mapUiSettings.setCompassEnabled(true);
 
 
     }
